ba8d7cdc20da277329b1866fb8f1d8294a89bf34
Ryoma10 <110236912+Ryoma10@users.noreply.github.com>
2022-08-19 13:41:59 +0000

    [Update]モデル完成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,19 @@
 class Order < ApplicationRecord
+
+  belongs_to :customer
+  has_many :order_details, dependent: :destroy
+
+  enum payment_method: {
+    credit: 0,  #クレジットカード
+    bank: 1,  #銀行振込
+  }
+
+  enum status: {
+    waiting: 0, #入金待ち
+    check: 1, #入金確認
+    making: 2,  #製作中
+    preparation: 3,  #発送準備
+    shipped_out: 4, #発送済み
+  }
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,13 @@
 class OrderDetail < ApplicationRecord
+
+  belongs_to :item
+  belongs_to :order
+
+  enum making_status: {
+    not_allowed: 0, #製作不可
+    waiting: 1, #製作待ち
+    making: 2,  #製作中
+    making_complete: 3,  #製作完了
+  }
+
 end
