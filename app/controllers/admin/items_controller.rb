372fd1e5aa3a4ead25aa1a0c5dfb5e44a095388f
Ryoma10 <110236912+Ryoma10@users.noreply.github.com>
2022-08-24 13:28:44 +0000

    [Update]商品一覧完成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,9 +7,23 @@ class Admin::ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
+  end
+
   def show
   end
 
   def edit
   end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
+
 end
