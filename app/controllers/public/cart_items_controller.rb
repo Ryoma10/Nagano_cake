da7eb0e5db31b74a4d6f58335fd3f6f796ad2729
Ryoma10 <110236912+Ryoma10@users.noreply.github.com>
2022-08-28 05:09:52 +0000

    [Update]public_item_show作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,11 @@
 class Public::CartItemsController < ApplicationController
+  def create
+    cart_item = Cart_item.new(cart_item)
+    cart_item.item_id =
+    cart_item.save
+    redirect_to cart_items_path(@item.id)
+  end
+
   def index
   end
 end
