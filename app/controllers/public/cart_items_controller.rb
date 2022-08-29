5521c34b57336a8186f548ac6556d60dd01e1c48
Ryoma10 <110236912+Ryoma10@users.noreply.github.com>
2022-08-29 14:36:59 +0000

    [Update]public_items_show完成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,29 @@
 class Public::CartItemsController < ApplicationController
   def create
-    cart_item = Cart_item.new(cart_item)
-    cart_item.item_id =
-    cart_item.save
-    redirect_to cart_items_path(@item.id)
+    @cart_item = CartItem.new(params_cart_item)
+    @cart_items = current_customer.cart_items.all
+      @cart_items.each do |cart_item|
+        if cart_item.item_id == @cart_item.item_id
+        new_amount = cart_item.amount + @cart_item.amount
+        cart_item.update_attribute(:amount, new_amount)
+        @cart_item.delete
+        end
+      end
+    @cart_item.save
+    redirect_to cart_items_path,notice: "カートに商品が入りました"
+
   end
 
   def index
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
   end
+
+
+  private
+
+  def params_cart_item
+    params.permit(:amount, :item_id, :customer_id)
+  end
+
 end
