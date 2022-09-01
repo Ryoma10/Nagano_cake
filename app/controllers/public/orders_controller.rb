b77be718e600ddc21cd34dc91dc353c5131b0b94
Ryoma10 <110236912+Ryoma10@users.noreply.github.com>
2022-09-01 15:12:05 +0000

    [Update]order_confirm作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,7 +4,22 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @cart_items = current_customer.cart_items.all
+  #  Order.shipping_cost(800)
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_of_price }
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    end
   end
+
 
   def complete
   end
@@ -14,4 +29,12 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
+
 end
