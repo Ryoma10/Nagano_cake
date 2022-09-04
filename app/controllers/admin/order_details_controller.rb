7031694bb7c439353cb811cbb6264691cd21d5a3
Ryoma10 <110236912+Ryoma10@users.noreply.github.com>
2022-09-04 05:18:05 +0000

    [Finish]テスト完了

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,17 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_details = @order.order_details
     @order_detail.update(order_detail_params)
+    if @order_details.where(making_status: "making").count >= 1
+      @order.status = "making"
+      @order.save
+    end
+    if @order.order_details.count == @order_details.where(making_status: "making_complete").count
+      @order.status = "preparation"
+      @order.save
+    end
     redirect_to admin_order_path(@order_detail.order.id)
   end
 
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,6 +8,15 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    @order_details = @order.order_details
+    if @order.status == "check"
+      @order_details.each do |order_detail|
+        order_detail.making_status = "waiting"
+        order_detail.save
+      end
+    end
+
+
     redirect_to admin_order_path(@order.id)
   end
 
